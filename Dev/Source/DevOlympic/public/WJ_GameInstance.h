24593aebc2c5e3f8546ede9faffeba3ee7c1c6e3
JustinKangDev <85150138+JustinKangDev@users.noreply.github.com>
2021-09-28 15:05:47 +0900

    LobbyPlayer 스크립트 수정, GameInstance 스크립트 생성

---
+++ Dev/Source/DevOlympic/public/WJ_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "WJ_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVOLYMPIC_API UWJ_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
