b7bf72792e8c647333f8de4efe535c8ea7adfc7c
woojooc <86417404+woojooc@users.noreply.github.com>
2021-09-28 15:56:12 +0900

    class 생성. 로비 작업 중

---
+++ Dev/Source/DevOlympic/public/WJ_LobbyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WJ_LobbyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVOLYMPIC_API AWJ_LobbyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
